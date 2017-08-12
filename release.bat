5daea50de526f735d04233342e176baeb487490e
Łukasz Klimek <klinn91@gmail.com>
2017-08-12 15:40:18 +0200

    added release script

---
+++ release.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+SET param_ver=%1
+SET param_label=%2
+SET ver_joined=%1-%2
+
+IF "%param_ver%" == "" (
+    ECHO Specify release version. 1>&2
+    EXIT 1
+)
+
+IF "%param_label%" == "" (
+    SET ver_joined=%1
+)
+
+ECHO Releasing the application with version %ver_joined%
+
+msbuild /p:Configuration=Release /p:AssemblyVersion=%param_ver% /p:AssemblyVersionSuffix=%param_label%
+nuget pack build/NPolyglot.Core.nuspec -properties configuration=Release;version=%ver_joined% -output build/
+nuget pack build/NPolyglot.LanguageDesign.nuspec -properties configuration=Release;version=%ver_joined% -output build/
